e20c1b86e4307558d3a59493420f8b7a982ec5d2
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-11 13:53:56 -0600

    add gitignore application properties

---
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/model/Product.java
@@ -0,0 +1,123 @@
+package com.pluralsight.NorthwindTradersAPI.model;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private int supplierId;
+    private int categoryId;
+    private String quantityPerUnit;
+    private Double unitPrice;
+    private int unitsInStock;
+    private int unitsOnOrder;
+    private int reorderLevel;
+    private boolean discontinued;
+
+    public Product(int productId, String productName, int supplierId, int categoryId, String quantityPerUnit, Double unitPrice, int unitsInStock, int unitsOnOrder, int reorderLevel, boolean discontinued) {
+        this.productId = productId;
+        this.productName = productName;
+        this.supplierId = supplierId;
+        this.categoryId = categoryId;
+        this.quantityPerUnit = quantityPerUnit;
+        this.unitPrice = unitPrice;
+        this.unitsInStock = unitsInStock;
+        this.unitsOnOrder = unitsOnOrder;
+        this.reorderLevel = reorderLevel;
+        this.discontinued = discontinued;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public int getUnitsOnOrder() {
+        return unitsOnOrder;
+    }
+
+    public void setUnitsOnOrder(int unitsOnOrder) {
+        this.unitsOnOrder = unitsOnOrder;
+    }
+
+    public int getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public void setReorderLevel(int reorderLevel) {
+        this.reorderLevel = reorderLevel;
+    }
+
+    public boolean isDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+
+    @Override
+    public String toString() {
+        return "\n Products: \n" +
+                " productId = " + productId + '\n' +
+                " productName = " + productName + '\n' +
+                " supplierID = " + supplierId + '\n' +
+                " categoryId = " + categoryId + '\n' +
+                " quantityPerUnit = " + quantityPerUnit + '\n' +
+                " unitPrice = " + unitPrice + '\n' +
+                " unitsInStock = " + unitsInStock + '\n' +
+                " unitsOnOrder = " + unitsOnOrder + '\n' +
+                " reorderLevel = " + reorderLevel + '\n' +
+                " discontinued = " + discontinued + '\n';
+    }
+
+}
