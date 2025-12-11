004b643017cdecf961f21ee6616faa03d15f4a73
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-11 09:21:07 -0600

    initial commit

---
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -0,0 +1,19 @@
+package com.pluralsight.NorthwindTradersAPI;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//HomeController homeController = new HomeController();
+//homeController.renderHomePage()
+
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String index(@RequestParam(defaultValue =  "World") String country){
+        return "Hello " + country;
+    }
+}
